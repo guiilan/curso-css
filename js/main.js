9fdfdd1e2fa99c58f105816daf611ec88d9d5555
Guilherme Ilan <gui.tioze@gmail.com>
2021-03-12 10:23:57 -0300

    animação do preloader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+//Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 
+//Page preloader
+window.addEventListener('load', function(){
+    var pagePreloader = document.querySelector(".jl-preloader");
+    setTimeout(function(){
+        pagePreloader.classList.add('jl-fade-out');
+    }, 3000)
+    
+});
+
+//Abrindo e Fechando Info de Contato
 btnContact.addEventListener('click', function(){
     var boxContact = document.querySelector('.jl-contact-info');
     
